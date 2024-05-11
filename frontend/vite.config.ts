a9835ccf535f6c9db7a46ef88788ec9c1ea4f8c4
Карачев Кирилл <umbrellamixmail@gmail.com>
2024-05-11 14:49:32 +0700

    add

---
+++ frontend/vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath } from "url";
+import { Alias, defineConfig } from "vite";
+
+const getAliasObject = (findName: string, path: string): Alias => {
+    return {
+        find: findName,
+        replacement: fileURLToPath(new URL(path, import.meta.url)),
+    };
+};
+
+export default defineConfig({
+    server: {
+      port: 4200,
+      proxy: {
+          "/api": {
+            //changeOrigin: true,
+            target: "http://127.0.0.1:4200",
+            changeOrigin: true,
+            secure: false,
+          }
+        }
+  },
+})
